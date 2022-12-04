1607a2e3763e2c3c7bbe40489cad6cc8d4cffc87
Adam Trepka <kleszczowiec@gmail.com>
2022-12-04 10:19:56 +0100

    add day 4 answers

--- AdventOfCode2022/AdventOfCode2022.Day.04/Solver.cs
+++ AdventOfCode2022/AdventOfCode2022.Day.04/Solver.cs
@@ -8,22 +8,60 @@ namespace AdventOfCode2022.Day._04
 
         public async Task<string> PartOne()
         {
+            var counter = 0;
             foreach (var line in await File.ReadAllLinesAsync("./202204.txt"))
             {
-                
+                var pairs = line.Split(",");
+
+                var firstRangeIds = pairs[0].Split("-").Select(int.Parse);
+                var secondRangeIds = pairs[1].Split("-").Select(int.Parse);
+
+                var firstRange = new Range(firstRangeIds.First(), firstRangeIds.Last());
+                var secondRange = new Range(secondRangeIds.First(), secondRangeIds.Last());
+
+                if (Contains(firstRange, secondRange) || Contains(secondRange,firstRange))
+                {
+                    counter++;
+                }
             }
 
-            throw new NotImplementedException();
+            return counter.ToString();
         }
 
         public async Task<string> PartTwo()
         {
+            var counter = 0;
             foreach (var line in await File.ReadAllLinesAsync("./202204.txt"))
             {
+                var pairs = line.Split(",");
 
+                var firstRangeIds = pairs[0].Split("-").Select(int.Parse);
+                var secondRangeIds = pairs[1].Split("-").Select(int.Parse);
+
+                var firstRange = new Range(firstRangeIds.First(), firstRangeIds.Last());
+                var secondRange = new Range(secondRangeIds.First(), secondRangeIds.Last());
+
+                if (Overlap(firstRange, secondRange) || Overlap(secondRange, firstRange))
+                {
+                    counter++;
+                }
             }
 
-            throw new NotImplementedException();
+            return counter.ToString();
         }
+
+        private bool Contains(Range firstRange, Range secondRange)
+        {
+            return firstRange.from <= secondRange.from && secondRange.to <= firstRange.to;
+        }
+
+        private bool Overlap(Range firstRange, Range secondRange)
+        {
+            return firstRange.to >= secondRange.from && firstRange.from <= secondRange.to;
+        }
+
+        private record struct Range(int from, int to);
     }
+
+
 }
