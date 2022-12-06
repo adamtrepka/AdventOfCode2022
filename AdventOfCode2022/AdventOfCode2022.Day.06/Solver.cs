ba297e249059e523adfc4760e0d4a322f805b304
Adam Trepka <kleszczowiec@gmail.com>
2022-12-06 06:53:24 +0100

    add day 6 answers

---
+++ AdventOfCode2022/AdventOfCode2022.Day.06/Solver.cs
@@ -0,0 +1,75 @@
+﻿using AdventOfCode2022.Common.Abstraction;
+
+namespace AdventOfCode2022.Day._06
+{
+    public class Solver : ISolver
+    {
+        public string Title => "2022 - ";
+
+        public async Task<string> PartOne()
+        {
+            var sizeOfPacket = 4;
+
+            var stream = await File.ReadAllTextAsync("./202206.txt");
+
+            var package = new List<char>();
+
+            var indexOfStartOfStream = 0;
+
+            foreach (var letter in stream)
+            {
+                indexOfStartOfStream++;
+                
+                if(package.Count == sizeOfPacket)
+                {
+                    package.RemoveAt(0);
+                }
+                
+                package.Add(letter);
+
+                if(package.Count == sizeOfPacket && package.GroupBy(x => x).All(x => x.Count() == 1))
+                {
+                    break;
+                }
+            }
+
+
+            return indexOfStartOfStream.ToString();
+        }
+
+        public async Task<string> PartTwo()
+        {
+            
+            var sizeOfMessage = 14;
+
+            var stream = await File.ReadAllTextAsync("./202206.txt");
+
+            var package = new List<char>();
+
+            var indexOfStartOfStream = int.Parse(await PartOne());
+            var indexOfStartMessage = 0;
+
+            foreach (var letter in stream.Skip(indexOfStartOfStream))
+            {
+                indexOfStartMessage++;
+
+                if (package.Count == sizeOfMessage)
+                {
+                    package.RemoveAt(0);
+                }
+
+                package.Add(letter);
+
+                if (package.Count == sizeOfMessage && package.GroupBy(x => x).All(x => x.Count() == 1))
+                {
+                    break;
+                }
+            }
+
+
+            return (indexOfStartOfStream + indexOfStartMessage).ToString();
+        }
+    }
+
+
+}
