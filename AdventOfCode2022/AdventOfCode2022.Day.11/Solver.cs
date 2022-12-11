3d86ba2a12c946dc0bd4d6a99a0de7922510e339
Adam Trepka <kleszczowiec@gmail.com>
2022-12-11 09:14:16 +0100

    add day 11 answers

---
+++ AdventOfCode2022/AdventOfCode2022.Day.11/Solver.cs
@@ -0,0 +1,151 @@
+﻿using AdventOfCode2022.Common;
+using AdventOfCode2022.Common.Abstraction;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AdventOfCode2022.Day._11
+{
+    public class Solver : ISolver
+    {
+        private readonly ITextFileReader _textFileReader;
+
+        public Solver(ITextFileReader textFileReader)
+        {
+            _textFileReader = textFileReader ?? throw new ArgumentNullException(nameof(textFileReader));
+        }
+
+        public string Title => "2022 - Day 11: Monkey in the Middle";
+
+        public async Task<string> PartOne()
+        {
+            var input = await _textFileReader.ReadAllTextAsync("./202211.txt");
+
+            var setups = input.Split("\r\n\r\n");
+            var monkeys = setups.Select(ParseSetup).ToList();
+
+            for (int i = 0; i < 20; i++)
+            {
+                Round(monkeys, item => item / 3);
+            }
+
+            var monkeyBusiness = monkeys.OrderByDescending(x => x.InspectedItems)
+                                        .Take(2)
+                                        .Aggregate(1L, (value, monkey) => value * monkey.InspectedItems);
+
+            return monkeyBusiness.ToString();
+        }
+
+        public async Task<string> PartTwo()
+        {
+            var input = await _textFileReader.ReadAllTextAsync("./202211.txt");
+
+            var setups = input.Split("\r\n\r\n");
+
+            var monkeys = setups.Select(ParseSetup).ToList();
+
+            var mod = monkeys.Aggregate(1, (mod, monkey) => mod * monkey.Modulo);
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                Round(monkeys, item => item % mod);
+            }
+
+            var monkeyBusiness = monkeys.OrderByDescending(x => x.InspectedItems)
+                                        .Take(2)
+                                        .Aggregate(1L, (value, monkey) => value * monkey.InspectedItems);
+
+            return monkeyBusiness.ToString();
+        }
+
+        private Monkey ParseSetup(string setup)
+        {
+            var lines = setup.Split("\n").Select(x => x.Trim());
+
+            var monkey = new Monkey();
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("Monkey"))
+                {
+                    var value = int.Parse(line.Replace("Monkey", "").Replace(":", ""));
+                    monkey.Index = value;
+                }
+                else if (line.StartsWith("Starting items:"))
+                {
+                    var items = line.Replace("Starting items:", "").Split(",").Select(x => long.Parse(x)).ToList();
+                    monkey.Items = new Queue<long>(items);
+                }
+                else if (line.StartsWith("Operation: new = old * old"))
+                {
+                    monkey.Operation = (input) => input * input;
+                }
+                else if (line.StartsWith("Operation: new = old *"))
+                {
+                    var value = long.Parse(line.Replace("Operation: new = old *", ""));
+
+                    monkey.Operation = (input) => input * value;
+                }
+                else if (line.StartsWith("Operation: new = old +"))
+                {
+                    var value = long.Parse(line.Replace("Operation: new = old +", ""));
+
+                    monkey.Operation = (input) => input + value;
+                }
+                else if (line.StartsWith("Test: divisible by "))
+                {
+                    var value = int.Parse(line.Replace("Test: divisible by ", ""));
+                    monkey.Modulo = value;
+                }
+                else if (line.StartsWith("If true: throw to monkey "))
+                {
+                    var value = int.Parse(line.Replace("If true: throw to monkey ", ""));
+                    monkey.ThrowToMonkeyIfTrue = value;
+                }
+                else if (line.StartsWith("If false: throw to monkey "))
+                {
+                    var value = int.Parse(line.Replace("If false: throw to monkey ", ""));
+                    monkey.ThrowToMonkeyIfFalse = value;
+                }
+            }
+
+            return monkey;
+        }
+
+        private void Round(List<Monkey> monkeys, Func<long, long> updateWorryLevel)
+        {
+            foreach (var monkey in monkeys)
+            {
+                while (monkey.Items.Any())
+                {
+                    monkey.InspectedItems++;
+
+                    var item = monkey.Items.Dequeue();
+                    item = monkey.Operation(item);
+                    item = updateWorryLevel(item);
+
+                    int targetMonkey = monkey.CheckTheTargetMonkey(item);
+
+                    monkeys[targetMonkey].Items.Enqueue(item);
+                }
+            }
+        }
+    }
+
+    internal class Monkey
+    {
+        public int Index { get; set; }
+        public Queue<long> Items { get; set; }
+        public Func<long, long> Operation { get; set; }
+        public int Modulo { get; set; }
+        public int ThrowToMonkeyIfTrue { get; set; }
+        public int ThrowToMonkeyIfFalse { get; set; }
+        public long InspectedItems { get; set; }
+        public int CheckTheTargetMonkey(long item)
+        {
+            return item % Modulo == 0 ? ThrowToMonkeyIfTrue : ThrowToMonkeyIfFalse;
+        }
+
+    }
+}
---
+++ AdventOfCode2022/AdventOfCode2022.Tests/Day/Day11.cs
@@ -0,0 +1,97 @@
+﻿using AdventOfCode2022.Common.Abstraction;
+using Moq;
+
+namespace AdventOfCode2022.Tests.Day
+{
+    [TestClass]
+    public class Day11
+    {
+        [TestMethod]
+        public async Task Part1_ForImputFromExample_ShouldReturn10605()
+        {
+            var fileReaderMock = new Mock<ITextFileReader>();
+            fileReaderMock.Setup(reader => reader.ReadAllTextAsync(It.IsAny<string>())).Returns(() =>
+            {
+                var result = "Monkey 0:\r\n" +
+                             "  Starting items: 79, 98\r\n" +
+                             "  Operation: new = old * 19\r\n" +
+                             "  Test: divisible by 23\r\n" +
+                             "    If true: throw to monkey 2\r\n" +
+                             "    If false: throw to monkey 3\r\n" +
+                             "\r\n" +
+                             "Monkey 1:\r\n" +
+                             "  Starting items: 54, 65, 75, 74\r\n" +
+                             "  Operation: new = old + 6\r\n" +
+                             "  Test: divisible by 19\r\n" +
+                             "    If true: throw to monkey 2\r\n" +
+                             "    If false: throw to monkey 0\r\n" +
+                             "\r\n" +
+                             "Monkey 2:\r\n" +
+                             "  Starting items: 79, 60, 97\r\n" +
+                             "  Operation: new = old * old\r\n" +
+                             "  Test: divisible by 13\r\n" +
+                             "    If true: throw to monkey 1\r\n" +
+                             "    If false: throw to monkey 3\r\n" +
+                             "\r\n" +
+                             "Monkey 3:\r\n" +
+                             "  Starting items: 74\r\n" +
+                             "  Operation: new = old + 3\r\n" +
+                             "  Test: divisible by 17\r\n" +
+                             "    If true: throw to monkey 0\r\n" +
+                             "    If false: throw to monkey 1";
+
+                return Task.FromResult(result);
+            });
+
+            var solver = new AdventOfCode2022.Day._11.Solver(fileReaderMock.Object);
+
+            var result = await solver.PartOne();
+
+            Assert.AreEqual("10605", result);
+        }
+
+        [TestMethod]
+        public async Task Part2_ForImputFromExample_ShouldReturn2713310158()
+        {
+            var fileReaderMock = new Mock<ITextFileReader>();
+            fileReaderMock.Setup(reader => reader.ReadAllTextAsync(It.IsAny<string>())).Returns(() =>
+            {
+                var result = "Monkey 0:\r\n" +
+                             "  Starting items: 79, 98\r\n" +
+                             "  Operation: new = old * 19\r\n" +
+                             "  Test: divisible by 23\r\n" +
+                             "    If true: throw to monkey 2\r\n" +
+                             "    If false: throw to monkey 3\r\n" +
+                             "\r\n" +
+                             "Monkey 1:\r\n" +
+                             "  Starting items: 54, 65, 75, 74\r\n" +
+                             "  Operation: new = old + 6\r\n" +
+                             "  Test: divisible by 19\r\n" +
+                             "    If true: throw to monkey 2\r\n" +
+                             "    If false: throw to monkey 0\r\n" +
+                             "\r\n" +
+                             "Monkey 2:\r\n" +
+                             "  Starting items: 79, 60, 97\r\n" +
+                             "  Operation: new = old * old\r\n" +
+                             "  Test: divisible by 13\r\n" +
+                             "    If true: throw to monkey 1\r\n" +
+                             "    If false: throw to monkey 3\r\n" +
+                             "\r\n" +
+                             "Monkey 3:\r\n" +
+                             "  Starting items: 74\r\n" +
+                             "  Operation: new = old + 3\r\n" +
+                             "  Test: divisible by 17\r\n" +
+                             "    If true: throw to monkey 0\r\n" +
+                             "    If false: throw to monkey 1";
+
+                return Task.FromResult(result);
+            });
+
+            var solver = new AdventOfCode2022.Day._11.Solver(fileReaderMock.Object);
+
+            var result = await solver.PartTwo();
+
+            Assert.AreEqual("2713310158", result);
+        }
+    }
+}
